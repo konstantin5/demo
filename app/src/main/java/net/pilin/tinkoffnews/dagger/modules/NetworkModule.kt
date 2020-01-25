9cbb1a740a6aa8360cd6caac5d87fcb80a998137
Konstantin V. Pilin <konstantin@pilin.net>
2020-01-25 19:49:36 +0300

    use simple response cache

--- app/src/main/java/net/pilin/tinkoffnews/dagger/modules/NetworkModule.kt
+++ app/src/main/java/net/pilin/tinkoffnews/dagger/modules/NetworkModule.kt
@@ -1,5 +1,6 @@
 package net.pilin.tinkoffnews.dagger.modules
 
+import android.content.Context
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import dagger.Module
 import dagger.Provides
@@ -9,6 +10,13 @@ import net.pilin.tinkoffnews.BuildConfig
 import net.pilin.tinkoffnews.core.TinkoffNewsApi
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.CacheControl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+import okhttp3.Cache
+import java.io.File
+
 
 @Module
 object NetworkModule {
@@ -23,11 +31,45 @@ object NetworkModule {
     @Provides
     @Reusable
     @JvmStatic
-    internal fun provideRetrofit(): Retrofit {
+    internal fun provideRetrofit(okhttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BuildConfig.HOST)
+            .client(okhttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
             .build()
+    }
+
+    @Provides
+    @Reusable
+    internal fun provideHttpCache(context: Context): Cache {
+        val httpCacheDirectory = File(context.cacheDir, "http-cache")
+        val cacheSize = 10 * 1024 * 1024L
+        return Cache(httpCacheDirectory, cacheSize)
+    }
+
+    @Provides
+    @Reusable
+    internal fun provideHttpClient(cache: Cache): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(CacheInterceptor())
+            .cache(cache)
+            .build()
+    }
+
+
+    class CacheInterceptor : Interceptor {
+
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            val response = chain.proceed(chain.request())
+            val cacheControl = CacheControl.Builder()
+                .maxAge(15, TimeUnit.MINUTES) // 15 minutes cache
+                .build()
+            return response.newBuilder()
+                .removeHeader("Pragma")
+                .removeHeader("Cache-Control")
+                .header("Cache-Control", cacheControl.toString())
+                .build()
+        }
     }
 }
